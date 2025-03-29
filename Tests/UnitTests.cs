d26eb7f7744d4b603d1dd2f7f0180754b5289b8c
Cecilia Beckerbauer <98666851+cbeckerbauer@users.noreply.github.com>
2025-03-28 21:10:53 -0500

    Update UnitTests.cs

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -1,32 +1,33 @@
-namespace GithubActionsLab;
-
-[TestClass]
-public class Addition
+namespace GithubActionsLab
 {
-	[TestMethod]
-	public void Add_Valid_Patino()
-	{
-		Assert.AreEqual(3, Program.Add("1", "2"));
-		Assert.AreEqual(5, Program.Add("3", "2"));
-		Assert.AreEqual(12, Program.Add("5", "7"));
-	}
+    [TestClass]
+    public class Addition
+    {
+        [TestMethod]
+        public void Add_Valid_Patino()
+        {
+            Assert.AreEqual(3, Program.Add("1", "2"));
+            Assert.AreEqual(5, Program.Add("3", "2"));
+            Assert.AreEqual(12, Program.Add("5", "7"));
+        }
 
-	[TestMethod]
-	public void Add_Invalid_Patino()
-	{
-		Assert.ThrowsException<FormatException>(() => Program.Add("1", "a"));
-		Assert.ThrowsException<FormatException>(() => Program.Add("a", "1"));
-		Assert.ThrowsException<FormatException>(() => Program.Add("a", "a"));
-	}
+        [TestMethod]
+        public void Add_Invalid_Patino()
+        {
+            Assert.ThrowsException<FormatException>(() => Program.Add("1", "a"));
+            Assert.ThrowsException<FormatException>(() => Program.Add("a", "1"));
+            Assert.ThrowsException<FormatException>(() => Program.Add("a", "a"));
+        }
 
-	[TestMethod]
-	public void Add_Null_Patino()
-	{
-		Assert.ThrowsException<ArgumentNullException>(() => Program.Add("1", null));
-		Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, "1"));
-		Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, null));
-	}
-}
+        [TestMethod]
+        public void Add_Null_Patino()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() => Program.Add("1", null));
+            Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, "1"));
+            Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, null));
+        }
+    }
+
     [TestClass]
     public class Subtraction
     {
@@ -144,3 +145,4 @@ public class Addition
             Assert.ThrowsException<ArgumentNullException>(() => Program.Power(null, null));
         }
     }
+}
